fd47ca9d6906a8187b3d1fd44156397c746d5730
李永航 <85470879+bahayonghang@users.noreply.github.com>
2024-12-17 15:38:54 +0800

    feat: update backend main.py to include entry point and add .pyc files to .gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from ast import main
 from fastapi import FastAPI
 from app.routers import plot
 
@@ -9,3 +10,9 @@ app.include_router(plot.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Result Plotter API!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
